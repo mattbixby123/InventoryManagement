42e393d1f7d0870671349080e35f04a6dc38c870
mattbixby123 <matthew.bixby1@gmail.com>
2024-09-26 19:53:07 -0400

    saving progress ... mid redux installations

--- client/src/app/dashboard/dashboardWrapper.tsx
+++ client/src/app/dashboard/dashboardWrapper.tsx
@@ -1,18 +1,40 @@
-import React from 'react'
+'use client';
+
+import React, { useEffect } from 'react'
 import Navbar from '../(components)/Navbar';
 import Sidebar from '../(components)/Sidebar';
+import StoreProvider, { useAppSelector } from "../redux";
+
+const DashboardLayout = ({ children } : {children: React.ReactNode}) => {
+  const isSidebarCollapsed = useAppSelector((state) => state.global.isSidebarCollapsed);
+
+  const isDarkMode = useAppSelector((state) => state.global.isDarkMode);
+  
+  useEffect(() => {
+    if (isDarkMode) {
+      document.documentElement.classList.add("dark")
+    } else {
+      document.documentElement.classList.add("light");
+    }
+  })
+  return (
+    <div className={`${isDarkMode ? "dark" : "light"} flex bg-gray-50 text-gray-900 w-full min-h-screen`}>
+      <Sidebar />
+      <main className={`flex flex-col w-full h-full py-7 px-9 bg-gray-50 ${isSidebarCollapsed ? "md:pl-24" : "md:pl-72"}`}>
+        <Navbar />
+        {children}
+      </main>
+    </div>
+  );
+};
 
 
 
 const DashboardWrapper = ({ children } : {children: React.ReactNode}) => {
   return (
-    <div className={`light flex bg-gray-50 text-gray-900 w-full min-h-screen`}>
-      <Sidebar />
-      <main className={`flex flex-col w-full h-full py-7 px-9 bg-gray-50 md:pl-24`}>
-        <Navbar />
-        {children}
-      </main>
-    </div>
+    <StoreProvider>
+        <DashboardLayout>{children}</DashboardLayout>
+    </StoreProvider>
   );
 };
 
