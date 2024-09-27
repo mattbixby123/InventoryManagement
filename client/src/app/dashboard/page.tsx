b59d5637fac78823c96d3357dabef430acf014f8
mattbixby123 <matthew.bixby1@gmail.com>
2024-09-27 13:36:35 -0400

    backend dashboard get route is being used to format the dashbaord. WIP

--- client/src/app/dashboard/page.tsx
+++ client/src/app/dashboard/page.tsx
@@ -1,6 +1,14 @@
+'use client'
+
+
 const Dashboard = () => {
   return (
-    <div>Dashboard</div>
+    <div 
+    className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 
+      xl:overflow-auto gap-10 pb-4 custom-grid-rows"
+    >
+      <div className="row-span-3 xl:row-span-6 bg-gray-500" />
+    </div>
   )
 }
 
---
+++ server/src/index.ts
@@ -0,0 +1,28 @@
+import express from "express";
+import dotenv from "dotenv";
+import bodyParser from "body-parser";
+import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
+/* ROUTE IMPORTS */
+import dashboardRoutes from "./routes/dashboardRoutes"
+
+/* CONFIGURATIONS */
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+
+/* ROUTES */
+app.use("/dashboard", dashboardRoutes); 
+
+/* SERVER */
+const port = process.env.PORT || 3001;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
