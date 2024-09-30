b75e03a12a610cbe345c3daa049e8a40e8a9fb95
mattbixby123 <matthew.bixby1@gmail.com>
2024-09-30 15:21:19 -0400

    client/src/app/dashboard/CardSalesSummary.tsx complete

--- client/src/app/dashboard/page.tsx
+++ client/src/app/dashboard/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import CardPopularProducts from "./CardPopularProducts"
+import CardSalesSummary from "./CardSalesSummary"
 
 
 const Dashboard = () => {
@@ -10,7 +11,7 @@ const Dashboard = () => {
       xl:overflow-auto gap-10 pb-4 custom-grid-rows"
     >
       <CardPopularProducts />
-      <div className="row-span-3 xl:row-span-6 bg-gray-500" />
+      <CardSalesSummary />
       <div className="row-span-2 xl:row-span-3 col-span-1 md:col-span-2 xl:col-span-1 bg-gray-500" /> 
       <div className="row-span-3 bg-gray-500" />
       <div className="row-span-1 xl:row-span-2 bg-gray-500" />
