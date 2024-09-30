9b6a4677fb1627484da1828e8ef31310d30e459d
mattbixby123 <matthew.bixby1@gmail.com>
2024-09-30 12:27:03 -0400

    card popular products dashboard page finished for now

--- client/src/app/dashboard/CardPopularProducts.tsx
+++ client/src/app/dashboard/CardPopularProducts.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { useGetDashboardMetricsQuery } from '../state/api'
+import { ShoppingBag } from 'lucide-react';
+import Rating from '../(components)/Rating';
 
 const CardPopularProducts = () => {
   const { data: dashboardMetrics, isLoading } = useGetDashboardMetricsQuery();
@@ -20,16 +22,29 @@ const CardPopularProducts = () => {
                 key={product.productId}
                 className="flex items-center justify-between gap-3 px-5 py-7 border-b"
               >
-                <div>img</div>
-                <div className='flex flex-col justify-between gap-1'>
-                  <div className='font-bold text-gray-700'>{product.name}</div>
-                  <div className='flex text-sm items-center'>
-                      <span className='font-bold text-blue-500 text-xs'>
-                        ${product.price}
-                      </span>
+                <div className='flex items-center gap-3'>
+                  <div>img</div>
+                  <div className='flex flex-col justify-between gap-1'>
+                    <div className='font-bold text-gray-700'>
+                      {product.name}
+                    </div>
+                    <div className='flex text-sm items-center'>
+                        <span className='font-bold text-blue-500 text-xs'>
+                          ${product.price}
+                        </span>
+                        <span className='mx-2'>|</span>
+                        <Rating rating={product.rating || 0} />
+                    </div>
                   </div>
                 </div>
 
+                {/* RIGHT SIDE OF POPULAR PRODUCTS */}
+                <div className='text-xs flex items-center'>
+                  <button className='p-2 rounded-full bg-blue-100 text-blue-600 mr-2'>
+                    <ShoppingBag className='w-4 h-4' />
+                  </button>
+                  {Math.round(product.stockQuantity / 1000)}k Sold
+                </div>
               </div>
             ))}
           </div>
