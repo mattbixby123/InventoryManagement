1e34f4aa25a76ff22d293b93cbcc41e05e75e1b5
mattbixby123 <matthew.bixby1@gmail.com>
2024-10-01 20:26:12 -0400

    settings page added

--- client/src/state/api.ts
+++ client/src/state/api.ts
@@ -49,11 +49,16 @@ export interface DashboardMetrics {
   expenseSummary: ExpenseSummary[];
   expenseByCategorySummary: ExpenseByCategorySummary[];
 }
+export interface User {
+  userId: string;
+  name: string;
+  email: string;
+}
 
 export const api = createApi({
   baseQuery: fetchBaseQuery({ baseUrl: process.env.NEXT_PUBLIC_API_BASE_URL }),
   reducerPath: "api",
-  tagTypes: ["DashboardMetrics", "Products"],
+  tagTypes: ["DashboardMetrics", "Products", "Users"],
   endpoints: (build) => ({
     getDashboardMetrics: build.query<DashboardMetrics, void>({
       query: () => "/dashboard",
@@ -73,7 +78,15 @@ export const api = createApi({
         body: newProduct
       }),
       invalidatesTags: ["Products"]
-    })
+    }),
+    getUsers: build.query<User[], void>({
+      query: () => "/users",
+      providesTags: ["Users"],
+    }),
+    // getExpensesByCategory: build.query<ExpenseByCategorySummary[], void>({
+    //   query: () => "/expenses",
+    //   providesTags: ["Expenses"],
+    // }),
   })
 });
 
@@ -81,4 +94,6 @@ export const {
   useGetDashboardMetricsQuery,
   useGetProductsQuery,
   useCreateProductMutation,
+  useGetUsersQuery,
+  // useGetExpensesByCategory,
 } = api;
